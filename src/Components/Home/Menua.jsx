8ea6140b31d89a4031194ec275d4884b8c550809
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-07-05 00:04:50 +0300

    fix the menu

--- src/Components/Home/Menua.jsx
+++ src/Components/Home/Menua.jsx
@@ -2,7 +2,6 @@ import { AiFillAndroid, AiFillApple } from "react-icons/ai";
 import { GrWindows } from "react-icons/gr";
 import { HiSquares2X2 } from "react-icons/hi2";
 import "./Menua.css";
-import useWindowSize from "./useWindowSize";
 import { useState } from "react";
 
 function Menua() {
@@ -12,78 +11,26 @@ function Menua() {
     setIsOpen(!isOpen);
   };
 
-  const size = useWindowSize();
 
   return (
-    <div>
-      {size.width > 980 ? (
-        <div className="big-screen-items">
-          <button className="button-48">
-            <span className="text">
-              <GrWindows className="logo" />
-              <p>Windows</p>
-            </span>
-          </button>
-          <button className="button-48">
-            <span className="text">
-              <AiFillApple className="logo" />
-              <p>Mac</p>
-            </span>
-          </button>
-          <button className="button-48">
-            <span className="text">
-              <AiFillAndroid className="logo" />
-              <p>Android</p>
-            </span>
-          </button>
-          <button className="button-48">
-            <span className="text">
-              <HiSquares2X2 className="logo" />
-              <p>Categories</p>
-            </span>
-          </button>
-        </div>
-      ) : (
-        <div
-          className={`hamburger-menu ${isOpen ? "open" : ""}`}
-          onClick={toggleMenu}
-        >
-          <div className="hamburger-icon">
-            <span className={`line ${isOpen ? "line1" : ""}`}></span>
-            <span className={`line ${isOpen ? "line2" : ""}`}></span>
-            <span className={`line ${isOpen ? "line3" : ""}`}></span>
-          </div>
-          <div className="container">
-          <ul className={`menu-items ${isOpen ? "open" : "close"}`}>
-            <li className="button-48">
-              <span className="text">
-                <GrWindows className="logo" />
-                <p>Windows</p>
-              </span>
-            </li>
-            <li className="button-48">
-              <span className="text">
-                <AiFillApple className="logo" />
-                <p>Mac</p>
-              </span>
-            </li>
-            <li className="button-48">
-              <span className="text">
-                <AiFillAndroid className="logo" />
-                <p>Android</p>
-              </span>
-            </li>
-            <li className="button-48">
-              <span className="text">
-                <HiSquares2X2 className="logo" />
-                <p>Categories</p>
-              </span>
-            </li>
-          </ul>
-        </div>
-        </div>
-      )}
-    </div>
+    <nav>
+      <button onClick={toggleMenu}>menu</button>
+      <ul className={isOpen ? 'open' : 'closed'}>
+        <li>
+          <GrWindows className="icon" />
+          <span className="icon-text">windows</span>
+        </li>
+        <li>
+          <AiFillApple className="icon" />
+          <span className="icon-text">mac</span>
+          
+        </li>
+        <li>
+          <AiFillAndroid className="icon" />
+          <span className="icon-text">android</span>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
